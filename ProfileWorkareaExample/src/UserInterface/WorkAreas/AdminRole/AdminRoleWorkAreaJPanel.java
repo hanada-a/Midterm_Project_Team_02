e5f3554b1ecf210a97b01f0bd8c1771a0c96c2df
hanada-a <hanada.a@northeastern.edu>
2025-10-24 23:14:59 +0900

    Implemented the complete "Manage Students" functionality.

--- ProfileWorkareaExample/src/UserInterface/WorkAreas/AdminRole/AdminRoleWorkAreaJPanel.java
+++ ProfileWorkareaExample/src/UserInterface/WorkAreas/AdminRole/AdminRoleWorkAreaJPanel.java
@@ -14,6 +14,7 @@ import Business.Business;
 import Business.UserAccounts.UserAccount;
 import UserInterface.WorkAreas.AdminRole.AdministerUserAccountsWorkResp.ManageUserAccountsJPanel;
 import UserInterface.WorkAreas.AdminRole.ManagePersonnelWorkResp.ManagePersonsJPanel;
+import UserInterface.WorkAreas.AdminRole.ManageStudentsWorkResp.ManageStudentsJPanel;
 
 import javax.swing.JPanel;
 import javax.swing.JOptionPane;
@@ -184,8 +185,10 @@ public class AdminRoleWorkAreaJPanel extends javax.swing.JPanel {
     }//GEN-LAST:event_jButton10ActionPerformed
 
     private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
-        // TODO add your handling code here:
-
+        // Navigate to Manage Students - Author: Akira Hanada
+        ManageStudentsJPanel msp = new ManageStudentsJPanel(business, CardSequencePanel);
+        CardSequencePanel.add("Manage Students", msp);
+        ((java.awt.CardLayout) CardSequencePanel.getLayout()).next(CardSequencePanel);
     }//GEN-LAST:event_jButton8ActionPerformed
 
     private void jButton6IdentifyEventsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6IdentifyEventsActionPerformed
