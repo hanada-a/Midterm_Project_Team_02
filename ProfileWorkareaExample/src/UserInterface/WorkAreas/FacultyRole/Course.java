786ed51ae51b53927d53f27d6e5765d21b8d4308
maxwellsowell <sowell.m@northeastern.edu>
2025-10-23 22:58:12 -0700

    added Course, updated branch from main
    figured out how to use IntelliJ to deal with merge conflicts. my bad for not pushing my branch into main previously, will do so from now

---
+++ ProfileWorkareaExample/src/UserInterface/WorkAreas/FacultyRole/Course.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UserInterface.WorkAreas.FacultyRole;
+
+/**
+ *
+ * @author maxwellsowell
+ */
+public class Course {
+    
+    private String courseId;
+    private String courseName;
+    private Integer credits;
+    private Boolean isCore; // true = core required class, false = elective class
+    
+    String semester;
+    
+}
